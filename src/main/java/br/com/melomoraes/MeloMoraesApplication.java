b88637bed37c334fb6faa4fda0b5569df6cb89ca
Fabio Gomes Sakiyama <fabio.sakiyama.ecore@agcocorp.com>
2020-12-05 18:41:19 -0300

    Initial commit

---
+++ src/main/java/br/com/melomoraes/MeloMoraesApplication.java
@@ -0,0 +1,13 @@
+package br.com.melomoraes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeloMoraesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeloMoraesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/melomoraes/MeloMoraesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.melomoraes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeloMoraesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
